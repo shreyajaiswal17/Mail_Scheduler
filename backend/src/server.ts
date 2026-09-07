12c395c8f8a5d4faeee6795183a96f5113ec5d6c
Shreya Jaiswal <shreyaa.we@gmail.com>
2026-09-07 22:28:44 +0530

    intialized backend with ts,redis, prisma

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Mail Scheduler API is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
